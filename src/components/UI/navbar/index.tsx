612af5913027ca998b9b2bd6fcbdf3b8bc41785c
Pardeep <pardeep.singh@copancs.com>
2023-12-12 19:13:05 +0530

    UI fixes for header

--- src/components/UI/navbar/index.tsx
+++ src/components/UI/navbar/index.tsx
@@ -1,8 +1,8 @@
-import { InputField } from "@/components/formFields";
 import React from "react";
 import ReuseButton from "../common/ReuseButton";
 import { useForm } from "react-hook-form";
-import { Box, Flex, Image, Text } from "@chakra-ui/react";
+import { Box, Flex, Image, Input, Text } from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
 
 const Navbar = () => {
   const { control } = useForm();
@@ -46,9 +46,34 @@ const Navbar = () => {
           fontWeight="fontWeights.medium"
           lineHeight="120%"
         >
-          <Text color="typoClr.PrimaryActions">Notes</Text>
+          <Flex
+            fontSize="13px"
+            color="typoClr.PrimaryActions"
+            gap="5px"
+            alignItems="center"
+          >
+            <Image src="/assets/images/icon-notes.png" alt="download"></Image>
+            <Text pt={2}>Notes</Text>
+          </Flex>
         </Flex>
-        <InputField name="Input" control={control} placeHolder="Search" />
+        <Box position="relative">
+          <Text
+            position="absolute"
+            left="5px"
+            top="50%"
+            zIndex="9999"
+            transform="translateY(-50%)"
+          >
+            <Image src="/assets/images/search-icon.png" alt="download"></Image>
+          </Text>
+          <Input
+            variant="filled"
+            placeholder="Search"
+            pl={10}
+            width="22vw"
+            background="#EBF1FA"
+          />
+        </Box>
         <ReuseButton
           fs={13}
           px={15}
